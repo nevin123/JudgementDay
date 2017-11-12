eb62e34f6956772f6f2a67760e858a6a55238f40
nevin123 <nevin-wouters@hotmail.com>
2017-11-12 20:06:25 +0100

    Initial Commit

---
+++ Assets/GodrayController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodrayController : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float MinOpacity = 0;
+    [Range(0, 1)]
+    public float MaxOpacity = 1;
+
+    public float OpacitySmoothing = 1;
+
+    [Space]
+    public float OffsetSpeed = 1f;
+
+    bool adding = true;
+    float godrayAlpha;
+    Material godrayMat;
+
+    void Start()
+    {
+        godrayMat = GetComponent<SpriteRenderer>().material;
+    }
+
+    void Update()
+    {
+        if (adding)
+        {
+            godrayAlpha += OpacitySmoothing * Time.deltaTime;
+            transform.position += Vector3.left * OffsetSpeed * Time.deltaTime;
+            if (godrayAlpha >= MaxOpacity)
+                adding = false;
+        } else
+        {
+            godrayAlpha -= OpacitySmoothing * Time.deltaTime;
+            transform.position -= Vector3.left * OffsetSpeed * Time.deltaTime;
+            if (godrayAlpha <= MinOpacity)
+                adding = true;
+        }
+
+        
+        Color _godrayColor = godrayMat.color;
+        _godrayColor.a = godrayAlpha;
+        godrayMat.color = _godrayColor;
+    }
+}
---
+++ Assets/TouchInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+	void Update()
+    {
+#if UNITY_IPHONE || UNITY_ANDROID || UNITY_EDITOR
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Click(Input.GetTouch(0).position);
+        }
+#endif
+#if UNITY_EDITOR || UNITY_EDITOR_WIN || UNITY_STANDALONE
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click(Input.mousePosition);
+        }
+#endif
+    }
+
+    void Click(Vector2 position)
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+        if (hit != null && hit.collider != null)
+        {
+            Debug.Log("I'm hitting " + hit.collider.name);
+        }
+    }
+}
